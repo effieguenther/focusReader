7d651b0df2d9b46c48fed657b64beb338fdb74bc
effieguenther <effiegguenther@gmail.com>
2023-04-30 21:36:50 -0400

    exciting progress

--- background.js
+++
@@ -1,7 +0,0 @@
-console.log("background running")
-
-chrome.runtime.onMessage.addListener(gotMessage) 
-
-function gotMessage(msg, sender, sendResponse) {
-    console.log(message.txt);
-};
--- popup.js
+++ popup.js
@@ -1,11 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-    HALF_BOLD_SWITCH = document.querySelector('#halfBold') 
-    if (HALF_BOLD_SWITCH) {console.log("switch located")};
+    const HALF_BOLD_SWITCH = document.querySelector('#halfBold'); 
+    
+    //load the saved state of the checkbox
+    //requires storage permission in manifest.json
+    chrome.storage.sync.get("halfBoldEnabled", (data) => {
+        HALF_BOLD_SWITCH.checked = data.halfBoldEnabled;
+    });
 
-    HALF_BOLD_SWITCH.addEventListener('click', () => {
+    HALF_BOLD_SWITCH.addEventListener("change", () => {
+        //save the state of the checkbox
+        chrome.storage.sync.set({halfBoldEnabled: HALF_BOLD_SWITCH.checked});
+        
+        //locate the current tab and use programmatic injection to call a function which executes within the tab, and not within the isolated popup window
         chrome.tabs.query({active: true}, (tabs) => {
             const tab = tabs[0];
             if (tab) {
+                //requrires scripting permission in manifest.json
                 chrome.scripting.executeScript(
                     {
                         target:{tabId: tab.id, allFrames: true},

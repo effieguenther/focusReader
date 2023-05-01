aadaf1933452f3c5ed18a8621336bfc88352e3e6
effieguenther <effiegguenther@gmail.com>
2023-04-30 22:26:40 -0400

    half bold function works

--- popup.js
+++ popup.js
@@ -3,18 +3,21 @@ document.addEventListener("DOMContentLoaded", () => {
     
     //load the saved state of the checkbox
     //requires storage permission in manifest.json
-    chrome.storage.sync.get("halfBoldEnabled", (data) => {
+    /*chrome.storage.sync.get("halfBoldEnabled", () => {
         HALF_BOLD_SWITCH.checked = data.halfBoldEnabled;
     });
+    console.log(data.halfBoldEnabled);*/
 
     HALF_BOLD_SWITCH.addEventListener("change", () => {
         //save the state of the checkbox
-        chrome.storage.sync.set({halfBoldEnabled: HALF_BOLD_SWITCH.checked});
+        /*chrome.storage.sync.set({halfBoldEnabled: HALF_BOLD_SWITCH.checked});
+        console.log(data.halfBoldEnabled);*/
         
         //locate the current tab and use programmatic injection to call a function which executes within the tab, and not within the isolated popup window
         chrome.tabs.query({active: true}, (tabs) => {
             const tab = tabs[0];
             if (tab) {
+                if (HALF_BOLD_SWITCH.checked) {
                 //requrires scripting permission in manifest.json
                 chrome.scripting.executeScript(
                     {
@@ -23,6 +26,15 @@ document.addEventListener("DOMContentLoaded", () => {
                     },
                     
                 )
+                } else {
+                    chrome.scripting.executeScript(
+                        {
+                            target:{tabId: tab.id, allFrames: true},
+                            func: returnToDefault
+                        },
+                        
+                    ) 
+                }
             } else {
                 alert("there are no active tabs");
             }
@@ -30,7 +42,25 @@ document.addEventListener("DOMContentLoaded", () => {
      });
 
      function halfBoldFunction() {
-        console.log("function called");
-        document.querySelector("body").style.color = "red";
+        console.log("halfBoldFunction called");
+        const PARAGRAPHS = document.querySelectorAll("p");
+        
+        for (let i = 0; i < PARAGRAPHS.length; i++) {
+            const WORDS = PARAGRAPHS[i].textContent.split(" ");
+
+            for (let i = 0; i < WORDS.length; i++) {
+                const word = WORDS[i];
+                const halfLength = Math.ceil(word.length / 2);
+                const firstHalf = word.slice(0, halfLength);
+                const secondHalf = word.slice(halfLength);
+                WORDS[i] = `<b>${firstHalf}</b>${secondHalf}`;
+            }
+            PARAGRAPHS[i].innerHTML = WORDS.join(" ");
+        }  
+     }
+
+     function returnToDefault() {
+        console.log("returnToDefault called");
+        document.querySelector("body").style.color = "";
      }
 });

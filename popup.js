892cfd89832fff919d3c66d6e338e52518943374
effieguenther <effiegguenther@gmail.com>
2023-05-03 22:33:36 -0400

    remembering state of checkbox

--- popup.js
+++ popup.js
@@ -1,24 +1,25 @@
+//run this file only when the popup is opened
 document.addEventListener("DOMContentLoaded", () => {
     const HALF_BOLD_SWITCH = document.querySelector('#halfBold'); 
     
     //load the saved state of the checkbox
     //requires storage permission in manifest.json
-    /*chrome.storage.sync.get("halfBoldEnabled", () => {
+    chrome.storage.local.get("halfBoldEnabled", (data) => {
         HALF_BOLD_SWITCH.checked = data.halfBoldEnabled;
+        console.log(data.halfBoldEnabled);
     });
-    console.log(data.halfBoldEnabled);*/
 
     HALF_BOLD_SWITCH.addEventListener("change", () => {
         //save the state of the checkbox
-        /*chrome.storage.sync.set({halfBoldEnabled: HALF_BOLD_SWITCH.checked});
-        console.log(data.halfBoldEnabled);*/
+        chrome.storage.local.set({halfBoldEnabled: HALF_BOLD_SWITCH.checked});
+        console.log(HALF_BOLD_SWITCH.halfBoldEnabled);
         
         //locate the current tab and use programmatic injection to call a function which executes within the tab, and not within the isolated popup window
+        //requrires scripting permission in manifest.json
         chrome.tabs.query({active: true}, (tabs) => {
             const tab = tabs[0];
             if (tab) {
                 if (HALF_BOLD_SWITCH.checked) {
-                //requrires scripting permission in manifest.json
                 chrome.scripting.executeScript(
                     {
                         target:{tabId: tab.id, allFrames: false},
